5ced48db89ad96adbb92410fa0633bbaa8f1497d
real2two <26689496+real2two@users.noreply.github.com>
2024-09-19 16:44:47 -0400

    Gave up trying to support WebSockets

--- apps/server/src/proxy/index.ts
+++ apps/server/src/proxy/index.ts
@@ -11,7 +11,7 @@ export const proxy = new Hono();
 const route = "/api/proxy/:promptId/*";
 
 proxy.use(route, async (c, next) => {
-  const { proxyHref, proxyWebsocket } = await getProxy(c);
+  const { proxyHref } = await getProxy(c);
   return secureHeaders({
     originAgentCluster: "",
     contentSecurityPolicy: {
@@ -20,7 +20,7 @@ proxy.use(route, async (c, next) => {
       styleSrc: ["'self'", "'unsafe-inline'", "blob:"],
       imgSrc: ["'self'", "blob:", "data:"],
       fontSrc: ["'self'", "data:"],
-      connectSrc: [...developmentCsp, proxyHref, proxyWebsocket, "data:", "blob:"],
+      connectSrc: [...developmentCsp, proxyHref, "data:", "blob:"],
       mediaSrc: ["'self'", "blob:", "data:"],
       frameSrc: [...developmentCsp, proxyHref],
       childSrc: [...developmentCsp, proxyHref, "blob:"],
@@ -34,14 +34,21 @@ proxy.use(route, async (c, next) => {
 proxy.all(route, async (c) => {
   const { url } = await getProxy(c);
   try {
+    // Handle fetch
     const res = await fetch(url, {
       method: c.req.method,
-      headers: { "user-agent": "Someone Says" },
+      body: c.req.raw.body,
+      headers: {
+        ...c.req.raw.headers,
+        "user-agent": "Someone Says",
+      },
     });
+    if (res.status === 101) return c.newResponse(null);
 
-    return new Response(res.body, {
+    return c.newResponse(res.body, {
       status: res.status,
       headers: {
+        ...res.headers,
         "access-control-allow-origin": "*",
         "content-type": res.headers.get("content-type") ?? "",
       },
@@ -60,18 +67,21 @@ async function getProxy(c: Context<BlankEnv, typeof route>) {
     prompt.urlType === PromptType.Original ? c.req.path.slice(promptId.length + 12) : c.req.path;
 
   const http = prompt.urlSecure ? "https" : "http";
+  // const ws = prompt.urlSecure ? "wss" : "ws";
+
+  const absolutePath = `/api/proxy/${prompt.id}/`;
   const query = Object.entries(c.req.query()).length
     ? `?${new URLSearchParams(c.req.query())}`
     : "";
 
-  const absolutePath = `/api/proxy/${prompt.id}/`;
-
   const href = `${http}://${prompt.urlHost}`;
-  const proxyHref = `${env.Domain}${absolutePath}`;
-  const proxyWebsocket = `${env.Websocket}${absolutePath}`;
-
   const url = `${href}${path}${query}`;
+  const proxyHref = `${env.Domain}${absolutePath}`;
   // const proxyUrl = `${proxyHref}${path}${query}`;
 
-  return { proxyHref, proxyWebsocket, url };
+  // const websocketHref = `${ws}://${prompt.urlHost}`;
+  // const proxyWebsocket = `${env.Websocket}${absolutePath}`;
+  // const urlWebsocket = `${websocketHref}${path}${query}`;
+
+  return { proxyHref, url };
 }
