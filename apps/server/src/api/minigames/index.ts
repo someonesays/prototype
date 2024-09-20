8bb7c555de698426b15b08b893934438e7c8d717
real2two <26689496+real2two@users.noreply.github.com>
2024-09-20 11:01:07 -0400

    Added missing await

--- apps/server/src/api/minigames/index.ts
+++ apps/server/src/api/minigames/index.ts
@@ -7,7 +7,7 @@ export const minigames = new Hono();
 minigames.get("/:id", async (c) => {
   const id = c.req.param("id");
 
-  const minigame = getMinigamePublic(id);
+  const minigame = await getMinigamePublic(id);
   if (!minigame) return c.json({ code: MessageCodes.NotFound }, 404);
 
   return c.json(minigame);
