c9559f4cf78051ffff57a36d2aec350a3edc77df
real2two <26689496+real2two@users.noreply.github.com>
2024-09-30 15:54:19 -0400

    Update index.ts

--- apps/server/src/proxy/index.ts
+++ apps/server/src/proxy/index.ts
@@ -50,7 +50,6 @@ proxy.all(route, async (c) => {
       headers: {
         ...res.headers,
         "access-control-allow-origin": "*",
-        "access-control-expose-headers": "*",
         "content-type": res.headers.get("content-type") ?? "",
       },
     });
