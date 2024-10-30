74aa99ec05601fed9364adeaf913ba5c500aaae1
real2two <26689496+real2two@users.noreply.github.com>
2024-10-30 14:50:15 -0400

    Adding more WIP comments for the future

--- apps/client/src/routes/(rooms)/rooms/[roomId]/+page.svelte
+++ apps/client/src/routes/(rooms)/rooms/[roomId]/+page.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
 import MinigameContainer from "$lib/components/elements/MinigameContainer.svelte";
+
+// WIP: Add a warning if you try to navigate out of the page
+/*
+  import { beforeNavigate } from "$app/navigation";
+
+  beforeNavigate(({ cancel }) => {
+    cancel();
+  });
+*/
 </script>
   
 <main>
