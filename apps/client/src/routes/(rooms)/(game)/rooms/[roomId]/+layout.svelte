f98acee0801a41cc8f503117e4092eb73328f57a
real2two <26689496+real2two@users.noreply.github.com>
2024-11-18 23:51:26 -0500

    Moving around frontend files

--- apps/client/src/routes/(rooms)/(game)/rooms/[roomId]/+layout.svelte
+++ apps/client/src/routes/(rooms)/(game)/rooms/[roomId]/+layout.svelte
@@ -3,30 +3,6 @@ let { children } = $props();
 </script>
 
 <style>
-  :root {
-    --primary: #ece2fc;
-    --secondary: #fafafa;
-    
-    --primary-button: #5812e2;
-    --success-button: #23a55a;
-    --error-button: #aa0e32;
-    --secondary-button: #4c5660;
-
-    --error-button-hover: #850725;
-
-    --primary-text: #000000;
-    --secondary-text: #242424;
-    
-    --card-stroke: #b3b3b3; 
-    --settings-button: #343a40;
-    --report-flag: #ff0000;
-
-    --bg-gradient-primary-1: #611ded;
-    --bg-gradient-primary-2: #89cad9;
-    --bg-gradient-secondary: #000000;
-    --bg-checkers: #ffffff14;
-    --bg-glow: #ff93f880;
-  }
   .black-bg {
     position: absolute;
     background: #000000;
