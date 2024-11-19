85ded5d173632d77c23c2bce37577a521c3f99fe
real2two <26689496+real2two@users.noreply.github.com>
2024-11-18 23:41:15 -0500

    Playing with background animations

--- apps/client/src/routes/(home)/+layout.svelte
+++ apps/client/src/routes/(home)/+layout.svelte
@@ -21,38 +21,112 @@ let { children } = $props();
     --settings-button: #343a40;
     --report-flag: #ff0000;
 
-    --bg-gradient-1: #611ded;
-    --bg-gradient-2: #89cad9;
+    --bg-gradient-primary-1: #611ded;
+    --bg-gradient-primary-2: #89cad9;
+    --bg-gradient-secondary: #000000;
     --bg-checkers: #ffffff14;
     --bg-glow: #ff93f880;
   }
-  .room {
+  .black-bg {
     position: absolute;
-    background: linear-gradient(90deg, var(--bg-gradient-1) 0%, var(--bg-gradient-2) 100%);
+    background: #000000;
     width: 100%;
     height: 100%;
   }
-  .pattern {
+  .container {
     position: absolute;
-    background-image: linear-gradient(45deg, var(--bg-checkers) 25%, transparent 25%), linear-gradient(-45deg, var(--bg-checkers) 25%, transparent 25%), linear-gradient(45deg, transparent 75%, var(--bg-checkers) 75%), linear-gradient(-45deg, transparent 75%, var(--bg-checkers) 75%);
-    background-size: 20px 20px;
-    background-position: 0 0, 0 10px, 10px -10px, -10px 0px;
     width: 100%;
     height: 100%;
+
+    /* animation-name: container-fade-out; */
+    animation-duration: 2s;
+    animation-fill-mode: forwards;
+  }
+  .child-container {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+  }
+  .primary-bg {
+    position: absolute;
+    background: linear-gradient(90deg, var(--bg-gradient-primary-1) 0%, var(--bg-gradient-primary-2) 100%);
+    width: 100%;
+    height: 100%;
+
+    /* animation-name: primary-bg-fade-out; */
+    animation-duration: 2s;
+    animation-fill-mode: forwards;
+  }
+  .pattern {
+    position: relative;
+    background: #ffffff;
+    filter: contrast(50) invert();
+    mix-blend-mode: lighten;
+    opacity: 10%;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+  .pattern::after {
+    content: '';
+    position: absolute;
+    inset: 0;
+    background-image: radial-gradient(circle at center, #000000 0%, transparent 100%);
+    background-size: 1rem 1rem;
+    background-repeat: round;
+    background-position: center;
+    mask-image: radial-gradient(#000000, #0000004d);
+    
+    /* animation-name: pattern-fade-out; */
+    animation-duration: 2s;
+    animation-fill-mode: forwards;
   }
   .glow {
     position: absolute;
-    background: radial-gradient(50% 50% at 50% 50%, var(--bg-glow) 0%, rgba(255, 255, 255, 0) 100%);
+    top: 0;
+    background: radial-gradient(50% 50% at 50% 50%, var(--bg-glow) 0%, #ffffff00 100%);
     width: 100%;
     height: 100%;
     overflow: auto;
   }
+  @keyframes container-fade-out {
+    from {
+      opacity: 100%;
+    }
+    to {
+      opacity: 0%;
+    }
+  }
+  @keyframes primary-bg-fade-out {
+    from {
+      opacity: 100%;
+    }
+    to {
+      opacity: 0%;
+    }
+  }
+  @keyframes pattern-fade-out {
+    0% {
+      background-color: #ffffff;
+      transform: scale(1);
+    }
+    100% {
+      background-color: #000000;
+      transform: scale(1.5);
+    }
+  }
 </style>
 
-<div class="room">
-  <div class="pattern">
-    <div class="glow">
-      {@render children()}
-    </div>
+<div class="black-bg"></div>
+
+<div class="container">
+  <div class="primary-bg">
+    <div class="glow"></div>
+  </div>
+  <div class="pattern"></div>
+
+  <div class="child-container">
+    {@render children()}
   </div>
 </div>
