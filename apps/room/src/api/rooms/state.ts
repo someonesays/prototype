b9fa29a2d0eab1dfc1a3204123e6ad70fb429ef8
real2two <26689496+real2two@users.noreply.github.com>
2024-11-27 03:03:23 -0500

    Add a race-condition check for resetting

--- apps/room/src/api/rooms/state.ts
+++ apps/room/src/api/rooms/state.ts
@@ -21,29 +21,39 @@ state.post("/", authMiddleware, zValidator("json", z.object({ maxRooms: z.number
 });
 
 // Kill all rooms
+let resetting = false;
 state.delete("/", authMiddleware, async (c) => {
   // Check if the server is already disabled
   const room = await getServerById(env.SERVER_ID);
-  if (!room) throw new Error("Cannot find room in the database");
+  if (!room) return c.json({ success: false, error: "Cannot find the room from the database" }, 500);
 
-  // Disable the server (if it's not already disabled)
-  if (!room.disabled) await setDisabled(true);
+  // Disallow race-conditioning this endpoint
+  if (resetting) return c.json({ success: false, error: "The server is currently resetting right now" }, 429);
+  resetting = true;
 
-  // Close all players
-  for (const room of rooms.values()) {
-    for (const player of room.players.values()) {
-      player.ws.close(1003, JSON.stringify({ code: ErrorMessageCodes.SERVER_SHUTDOWN }));
+  try {
+    // Disable the server (if it's not already disabled)
+    if (!room.disabled) await setDisabled(true);
+    // Close all players
+    for (const room of rooms.values()) {
+      for (const player of room.players.values()) {
+        player.ws.close(1003, JSON.stringify({ code: ErrorMessageCodes.SERVER_SHUTDOWN }));
+      }
     }
+    // Reset server started date (to check "iat" on the JWT)
+    resetServerStartedDate(Date.now());
+    // Enable the server (if it wasn't disabled originally)
+    if (!room.disabled) await setDisabled(false);
+
+    // Send response
+    resetting = false;
+    return c.json({ success: true });
+  } catch (err) {
+    // On failure, log the error and return a failure response
+    console.error(err);
+    resetting = false;
+    return c.json({ success: false, error: "An unexpected error has occurred" }, 500);
   }
-
-  // Reset server started date (to check "iat" on the JWT)
-  resetServerStartedDate(Date.now());
-
-  // Enable the server (if it wasn't disabled originally)
-  if (!room.disabled) await setDisabled(false);
-
-  // Send response
-  return c.json({ success: true });
 });
 
 // Get if a room exists
