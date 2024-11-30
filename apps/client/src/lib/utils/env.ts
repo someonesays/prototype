5d87b1ee02868cee228b139a3ffc354f747d79d7
real2two <26689496+real2two@users.noreply.github.com>
2024-11-29 22:00:25 -0500

    Refactored env as a hacky CF pages env fix

--- apps/client/src/lib/utils/env.ts
+++ apps/client/src/lib/utils/env.ts
@@ -1,17 +1,22 @@
-// export default {
-//   VITE_IS_PROD: import.meta.env.PROD,
-
-//   VITE_BASE_API: import.meta.env.VITE_BASE_API,
-//   VITE_DISCORD_CLIENT_ID: import.meta.env.VITE_DISCORD_CLIENT_ID,
-
-//   VITE_TURNSTILE_SITE_KEY: import.meta.env.VITE_TURNSTILE_SITE_KEY,
-// };
+const envs = {
+  development: {
+    VITE_BASE_API: import.meta.env.VITE_BASE_API,
+    VITE_DISCORD_CLIENT_ID: import.meta.env.VITE_DISCORD_CLIENT_ID,
+    VITE_TURNSTILE_SITE_KEY: import.meta.env.VITE_TURNSTILE_SITE_KEY,
+  },
+  staging: {
+    VITE_BASE_API: "https://staging-api.someonesays.io",
+    VITE_DISCORD_CLIENT_ID: "1312178792256507924",
+    VITE_TURNSTILE_SITE_KEY: "0x4AAAAAAA1Krb5_9TKRUaKy",
+  },
+  production: {
+    VITE_BASE_API: "",
+    VITE_DISCORD_CLIENT_ID: "",
+    VITE_TURNSTILE_SITE_KEY: "",
+  },
+};
 
 export default {
   VITE_IS_PROD: import.meta.env.PROD,
-
-  VITE_BASE_API: "https://staging-api.someonesays.io",
-  VITE_DISCORD_CLIENT_ID: "1312178792256507924",
-
-  VITE_TURNSTILE_SITE_KEY: "0x4AAAAAAA1Krb5_9TKRUaKy",
+  ...envs[import.meta.env.MODE as "development" | "staging" | "production"],
 };
