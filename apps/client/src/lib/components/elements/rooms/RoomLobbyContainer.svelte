ebc34c40de0cbdf94b68668a6d6e7b85e9d3472e
real2two <26689496+real2two@users.noreply.github.com>
2024-12-02 14:54:56 -0500

    Implemented the navbar of the room

--- apps/client/src/lib/components/elements/rooms/RoomLobbyContainer.svelte
+++ apps/client/src/lib/components/elements/rooms/RoomLobbyContainer.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 import Logo from "$lib/components/icons/Logo.svelte";
+import GearIcon from "$lib/components/icons/GearIcon.svelte";
+import DoorOpen from "$lib/components/icons/DoorOpen.svelte";
+
+import { volumeValue } from "$lib/components/stores/settings";
 import { launcher, launcherDiscordSdk } from "$lib/components/stores/launcher";
 import { room, roomWs } from "$lib/components/stores/roomState";
 import { ClientOpcodes } from "@/public";
 import { Permissions, PermissionUtils } from "@discord/embedded-app-sdk";
+
+let isSettingsOpen = $state(false);
 
 function setSettings(evt: SubmitEvent & { currentTarget: EventTarget & HTMLFormElement }) {
   evt.preventDefault();
@@ -70,125 +76,156 @@ function leaveGame() {
 </script>
 
 {#if $room}
-  <div class="nav-container">
-    <div class="nav">
-      <div class="left">
-        left
-      </div>
-      <div class="center">
-        <div class="logo">
-          <Logo />
+  <div class="app-container">
+    <div class="content-container">
+      <div class="nav-container">
+        <div class="left">
+          {#if $launcher === "normal"}
+            <button class="button leave" onclick={leaveGame}>
+              <div><DoorOpen /></div>
+            </button>
+          {/if}
+        </div>
+        <div class="center">
+          <div class="logo">
+            <Logo />
+          </div>
+        </div>
+        <div class="right">
+          <button class="button settings" class:active={isSettingsOpen} onclick={() => isSettingsOpen = !isSettingsOpen}>
+            <div><GearIcon /></div>
+          </button>
+          <div class="settings-menu" class:active={isSettingsOpen}>
+            <div>
+              <div>
+                <p class="volume-text-left">Volume</p>
+                <p class="volume-text-right">{$volumeValue}%</p>
+              </div>
+              <br>
+              <input class="volume-slider" type="range" min="0" max="100" bind:value={$volumeValue} />
+            </div>
+          </div>
         </div>
       </div>
-      <div class="right">
-        right
+      <div class="main-container">
+        <h2>Players</h2>
+        <ul>
+          {#each $room.players.sort((a, b) => b.points - a.points) as player}
+            <li>
+              <img src={player.avatar} width="50" height="50" alt="{player.displayName}'s avatar" />
+              {player.displayName}{$room.room.host === player.id ? " [HOST]" : ""}:
+              {player.points} point{player.points === 1 ? "": "s"}
+      
+              {#if $room.room.host === $room.user && $room.user !== player.id}
+                <button onclick={() => kickPlayer(player.id)}>
+                  Kick
+                </button>
+                <button onclick={() => transferHost(player.id)}>
+                  Transfer Host
+                </button>
+              {/if}
+            </li>
+          {/each}
+        </ul>
+      
+        <h2>Minigame information</h2>
+        <p>Pack: {$room.pack ? JSON.stringify($room.pack) : "None"}</p>
+        <p>Minigame: {$room.minigame ? JSON.stringify($room.minigame) : "None"}</p>
+      
+        {#if $room.pack?.iconImage}
+          <p>Pack icon image:</p>
+          <p>
+            <img alt="pack preview" src={
+              $launcher === "normal"
+                ? $room.pack.iconImage.normal
+                : $room.pack.iconImage.discord
+            } width="100" height="100" />
+          </p>
+        {/if}
+      
+        {#if $room.minigame?.previewImage}
+          <p>Minigame preview image:</p>
+          <p>
+            <img alt="minigame preview" src={
+              $launcher === "normal"
+                ? $room.minigame.previewImage.normal
+                : $room.minigame.previewImage.discord
+            } width="100" height="100" />
+          </p>
+        {/if}
+      
+        {#if $room.room.host === $room.user}
+          <form onsubmit={setSettings}>
+            <input type="text" name="pack_id" placeholder="Pack ID">
+            <input type="text" name="minigame_id" placeholder="Minigame ID">
+            <input type="submit" value="Set pack/minigame">
+          </form>
+        {/if}
+      
+        <h2>Actions</h2>
+        {#if $room.minigame && !$room.minigame.supportsMobile && $room.players.find(p => p.mobile)}
+          <p>WARNING: There is at least one mobile player in this lobby and this minigame doesn't support mobile devices!</p>
+        {/if}
+        <p>
+          <button onclick={copyInviteLink}>Invite</button>
+          <button onclick={startGame} disabled={$room.room.host !== $room.user}>Start</button>
+        </p>
       </div>
     </div>
   </div>
-
-  <h2>Players</h2>
-  <ul>
-    {#each $room.players.sort((a, b) => b.points - a.points) as player}
-      <li>
-        <img src={player.avatar} width="50" height="50" alt="{player.displayName}'s avatar" />
-        {player.displayName}{$room.room.host === player.id ? " [HOST]" : ""}:
-        {player.points} point{player.points === 1 ? "": "s"}
-
-        {#if $room.room.host === $room.user && $room.user !== player.id}
-          <button onclick={() => kickPlayer(player.id)}>
-            Kick
-          </button>
-          <button onclick={() => transferHost(player.id)}>
-            Transfer Host
-          </button>
-        {/if}
-      </li>
-    {/each}
-  </ul>
-
-  <h2>Minigame information</h2>
-  <p>Pack: {$room.pack ? JSON.stringify($room.pack) : "None"}</p>
-  <p>Minigame: {$room.minigame ? JSON.stringify($room.minigame) : "None"}</p>
-
-  {#if $room.pack?.iconImage}
-    <p>Pack icon image:</p>
-    <p>
-      <img alt="pack preview" src={
-        $launcher === "normal"
-          ? $room.pack.iconImage.normal
-          : $room.pack.iconImage.discord
-      } width="100" height="100" />
-    </p>
-  {/if}
-
-  {#if $room.minigame?.previewImage}
-    <p>Minigame preview image:</p>
-    <p>
-      <img alt="minigame preview" src={
-        $launcher === "normal"
-          ? $room.minigame.previewImage.normal
-          : $room.minigame.previewImage.discord
-      } width="100" height="100" />
-    </p>
-  {/if}
-
-  {#if $room.room.host === $room.user}
-    <form onsubmit={setSettings}>
-      <input type="text" name="pack_id" placeholder="Pack ID">
-      <input type="text" name="minigame_id" placeholder="Minigame ID">
-      <input type="submit" value="Set pack/minigame">
-    </form>
-  {/if}
-
-  <h2>Actions</h2>
-  {#if $room.minigame && !$room.minigame.supportsMobile && $room.players.find(p => p.mobile)}
-    <p>WARNING: There is at least one mobile player in this lobby and this minigame doesn't support mobile devices!</p>
-  {/if}
-  <p>
-    <button onclick={copyInviteLink}>Invite</button>
-    <button onclick={startGame} disabled={$room.room.host !== $room.user}>Start</button>
-    {#if $launcher === "normal"}
-      <button onclick={leaveGame}>Leave room</button>
-    {/if}
-  </p>
 {:else}
   <p>TODO: Make a loading screen animation of the lobby here!</p>
 {/if}
 
 <style>
+.app-container {
+  height: 100vh;
+  display: grid;
+  align-items: center;
+  vertical-align: middle;
+  overflow: auto;
+  overflow-wrap: anywhere;
+}
+.content-container {
+  margin: 4vh;
+}
 .nav-container {
   display: flex;
   align-content: center;
   justify-content: center;
 }
-.nav {
-  display: flex;
-  width: 90%;
-}
-.nav > .left {
+.nav-container > .left {
   flex: 1;
   display: flex;
   justify-content: flex-start;
   align-items: center;
 }
-.nav > .center {
+.nav-container > .center {
   flex: 1;
   display: flex;
   justify-content: center;
   align-items: center;
 }
-.nav > .right {
+.nav-container > .right {
+  position: relative;
   flex: 1;
   display: flex;
   justify-content: flex-end;
   align-items: center;
 }
+
 .logo {
-  width: calc(120px + 4vh);
+  width: calc(100px + 5vh);
+}
+
+.settings-menu {
+  position: absolute;
+  margin-top: 150px;
+  text-align: right;
 }
 
 @media only screen and (max-width: 480px) {
-  .nav {
+  .nav-container {
     display: flex;
     width: 100%;
   }
--- apps/client/src/lib/components/elements/rooms/RoomMinigameContainer.svelte
+++ apps/client/src/lib/components/elements/rooms/RoomMinigameContainer.svelte
@@ -100,7 +100,7 @@ function leaveOrEndGame() {
     </div>
   </div>
   <div class="minigame-settings">
-    <div class="minigame-settings-menu" class:minigame-settings-menu-active={isSettingsOpen}>
+    <div class="settings-menu" class:active={isSettingsOpen}>
       <div>
         <div>
           <p class="volume-text-left">Volume</p>
@@ -120,7 +120,7 @@ function leaveOrEndGame() {
         {/if}
       </div>
     </div>
-    <button class="minigame-settings-button" class:minigame-settings-button-active={isSettingsOpen} onclick={() => isSettingsOpen = !isSettingsOpen}>
+    <button class="button settings" class:active={isSettingsOpen} onclick={() => isSettingsOpen = !isSettingsOpen}>
       <div><GearIcon /></div>
     </button>
   </div>
@@ -218,93 +218,8 @@ function leaveOrEndGame() {
     right: calc(16px + 0.5vh);
     bottom: calc(16px + 0.5vh);
   }
-  .minigame-settings-menu {
-    display: none;
+  .settings-menu {
     margin-bottom: 12px;
-  }
-  .minigame-settings-menu p {
-    margin: 0;
-  }
-  .minigame-settings-menu-active {
-    display: block;
-    border-radius: 2px;
-    background-color: var(--settings-button);
-    border: 1px solid #242424;
-    box-shadow: #64646f33 0px 7px 29px 0px;
-    padding: 18px 12px;
-    width: 250px;
-    font-size: 16px;
-  }
-  .volume-text-left {
-    float: left;
-  }
-  .volume-text-right {
-    float: right;
-  }
-  .minigame-settings-button {
-    background-color: var(--settings-button);
-    border: 2px #4a5259 solid;
-    border-radius: calc(4px + 0.5vh);
-    box-shadow: #64646f33 0px 7px 29px 0px;
-    padding: 0px;
-    float: right;
-    cursor: pointer;
-    width: calc(40px + 0.5vh);
-    height: calc(40px + 0.5vh);
-    transition: box-shadow .5s ease-out;
-  }
-  .minigame-settings-button div {
-    margin: 25%;
-    width: 50%;
-    height: 50%;
-    transition: transform .5s ease-out;
-  }
-  .minigame-settings-button-active {
-    background-color: #4a5259;
-  }
-  .volume-slider {
-    --slider_activecolor: #1971c2;
-    --slider_linethickness: 4px;
-    --slider_thumbsize: 16px;
-    --slider_colormix: color-mix(in srgb, var(--slider_activecolor), #000 var(--slider_mixpercentage, 0%));
-
-    margin-top: 8px;
-    width: 100%;
-    height: var(--slider_thumbsize);
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    appearance: none;
-    background: none;
-    cursor: ew-resize;
-    overflow: hidden;
-  }
-  .volume-slider:focus-visible,
-  .volume-slider:hover {
-    --slider_mixpercentage: 25%;
-  }
-  .volume-slider[type="range" i]::-webkit-slider-thumb {
-    height: var(--slider_thumbsize);
-    aspect-ratio: 1;
-    border-radius: 50%;
-    box-shadow: 0 0 0 var(--slider_thumbsize, var(--slider_linethickness)) inset var(--slider_colormix);
-    border-image: linear-gradient(90deg, var(--slider_colormix) 50%, #ababab 0) 0 1/calc(50% - var(--slider_linethickness)/2) 100vw/0px calc(100vw);
-    -webkit-appearance: none;
-    appearance: none;
-  }
-  .volume-slider[type="range"]::-moz-range-thumb {
-    height: var(--slider_thumbsize);
-    width: var(--slider_thumbsize);
-    background: none;
-    border-radius: 50%;
-    box-shadow: 0 0 0 var(--slider_thumbsize, var(--slider_linethickness)) inset var(--slider_colormix);
-    border-image: linear-gradient(90deg, var(--slider_colormix) 50%, #ababab 0) 0 1/calc(50% - var(--slider_linethickness)/2) 100vw/0 calc(100vw);
-    -moz-appearance: none;
-    appearance: none;
-  }
-  @supports not (color: color-mix(in srgb, red, red)) {
-    .volume-slider {
-      --slider_colormix: var(--slider_activecolor);
-    }
   }
   .leave-button {
     background-color: var(--error-button);
@@ -318,14 +233,5 @@ function leaveOrEndGame() {
   }
   .leave-button:hover {
     background-color: var(--error-button-hover);
-  }
-  @media (hover: hover) {
-    .minigame-settings-button:hover {
-      background-color: #4a5259;
-      box-shadow: #64646f73 0px 7px 29px 0px;
-    }
-    .minigame-settings-button:hover > div {
-      transform: rotate(90deg);
-    }
   }
 </style>
