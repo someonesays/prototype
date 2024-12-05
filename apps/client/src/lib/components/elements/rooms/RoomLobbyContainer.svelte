3439b12a49d694f48c110f1ef49eda91881b473a
real2two <26689496+real2two@users.noreply.github.com>
2024-12-05 13:35:00 -0500

    yah hoverr

--- apps/client/src/lib/components/elements/rooms/RoomLobbyContainer.svelte
+++ apps/client/src/lib/components/elements/rooms/RoomLobbyContainer.svelte
@@ -570,8 +570,10 @@ function nextMinigameInPack() {
     background: var(--secondary-button);
     color: var(--primary);
   }
-  .action-button.invite:hover, .previousnext-button:hover {
-    background-color: #343a40;
+  @media (hover: hover), (-ms-high-contrast:none) {
+    .action-button.invite:hover, .previousnext-button:hover {
+      background-color: #343a40;
+    }
   }
   .action-button.invite:click, .previousnext-button:click {
     background-color: #343a40;
@@ -580,9 +582,10 @@ function nextMinigameInPack() {
     background: var(--success-button);
     color: var(--primary);
   }
-  .action-button.start:hover {
-    /* background-color: #4712b1; */
-    background-color: #19713e;
+  @media (hover: hover), (-ms-high-contrast:none) {
+    .action-button.start:hover {
+      background-color: #19713e;
+    }
   }
   @media (max-width: 319px) {
     .nav-container {
