ad30056c190b8216289a368222235ee10b639359
real2two <26689496+real2two@users.noreply.github.com>
2024-12-05 08:19:34 -0500

    attempt fix 2

--- apps/client/src/routes/(rooms)/(game)/rooms/[roomId]/+layout.svelte
+++ apps/client/src/routes/(rooms)/(game)/rooms/[roomId]/+layout.svelte
@@ -6,7 +6,7 @@ let { children } = $props();
   .container {
     position: absolute;
     background: linear-gradient(90deg, var(--bg-gradient-primary-1) 0%, var(--bg-gradient-primary-2) 100%);
-    background-position: contain;
+    background-attachment: fixed;
     width: 100%;
     height: 100%;
 
