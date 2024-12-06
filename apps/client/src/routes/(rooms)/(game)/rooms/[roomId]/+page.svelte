342652208514242b3fec1d6b86b9e003837e94ea
real2two <26689496+real2two@users.noreply.github.com>
2024-12-05 19:28:10 -0500

    oewntioegniogwke

--- apps/client/src/routes/(rooms)/(game)/rooms/[roomId]/+page.svelte
+++ apps/client/src/routes/(rooms)/(game)/rooms/[roomId]/+page.svelte
@@ -1,5 +1,17 @@
 <script>
+import { onMount } from "svelte";
+import { page } from "$app/stores";
+import { goto } from "$app/navigation";
+import { launcherMatchmaking } from "$lib/components/stores/launcher";
+
 import RoomContainer from "$lib/components/pages/RoomContainer.svelte";
+
+onMount(() => {
+  const roomId = $page.params.roomId;
+  if (!$launcherMatchmaking) return goto(roomId === "new" ? "/" : `/join/${encodeURIComponent(roomId)}`);
+});
 </script>
   
-<RoomContainer />
+{#if $launcherMatchmaking}
+  <RoomContainer />
+{/if}
