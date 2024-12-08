6ab10afb3e8d393eae4d983a582571426ee82e89
real2two <26689496+real2two@users.noreply.github.com>
2024-12-07 22:42:34 -0500

    discccc

---
+++ apps/client/src/routes/(auth)/auth/discord/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+import env from "$lib/utils/env";
+import { onMount } from "svelte";
+import { setCookie } from "$lib/utils/cookies.js";
+import { ErrorMessageCodes, ErrorMessageCodesToText } from "@/public";
+import { page } from "$app/stores";
+import { goto } from "$app/navigation";
+import { isModalOpen } from "$lib/components/stores/modal";
+import { kickedReason } from "$lib/components/stores/lobby";
+
+onMount(() => {
+  (async () => {
+    try {
+      const error = $page.url.searchParams.get("error");
+      if (error) return goto("/");
+
+      const res = await fetch(
+        `${env.VITE_BASE_API}/api/auth/discord/callback?code=${encodeURIComponent($page.url.searchParams.get("code") ?? "")}&state=${encodeURIComponent($page.url.searchParams.get("state") ?? "")}`,
+        { credentials: "include" },
+      );
+
+      const { code, authorization } = (await res.json()) as { code: ErrorMessageCodes; authorization: string };
+      if (!authorization) {
+        $isModalOpen = true;
+        $kickedReason = ErrorMessageCodesToText[code];
+        return goto("/");
+      }
+
+      setCookie("token", authorization);
+      return goto("/developers");
+    } catch (err) {
+      console.error(err);
+      return goto("/");
+    }
+  })();
+});
+</script>
