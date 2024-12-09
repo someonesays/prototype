f297abb89424071b5a1ba0aa8d25618efc3e50a9
real2two <26689496+real2two@users.noreply.github.com>
2024-12-09 03:06:19 -0500

    proper scale on those pages

--- apps/client/src/routes/(home)/(legal)/privacy/+page.svelte
+++ apps/client/src/routes/(home)/(legal)/privacy/+page.svelte
@@ -1,9 +1,28 @@
 <script>
 import BaseCard from "$lib/components/elements/cards/BaseCard.svelte";
+import { onMount } from "svelte";
+
+let transformScale = $state(1);
+
+onMount(() => {
+  const resize = () => {
+    if (window.innerWidth >= 1100 && window.innerHeight >= 660) {
+      transformScale = Math.min(window.innerWidth / 1100, window.innerHeight / 660);
+    } else {
+      transformScale = 1;
+    }
+  };
+  window.addEventListener("resize", resize);
+  resize();
+
+  return () => {
+    window.removeEventListener("resize", resize);
+  };
+});
 </script>
   
 <main class="misc-container">
-  <BaseCard style="width: 100%; max-width: 600px;">
+  <BaseCard style="width: 100%; max-width: 600px; transform: scale({transformScale});">
     <p><a class="url" href="/">Back</a></p>
 
     <h1>Privacy Policy - Someone Says</h1>
