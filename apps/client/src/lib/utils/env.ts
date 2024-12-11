575a512b10c7b5699f799e94bf71a583ca8ce376
real2two <26689496+real2two@users.noreply.github.com>
2024-12-11 02:50:07 -0500

    few

--- apps/client/src/lib/utils/env.ts
+++ apps/client/src/lib/utils/env.ts
@@ -16,8 +16,8 @@ const envs = {
   production: {
     VITE_BASE_FRONTEND: "https://someonesays.io",
     VITE_BASE_API: "https://api.someonesays.io",
-    VITE_DISCORD_CLIENT_ID: "",
-    VITE_TURNSTILE_SITE_KEY: "",
+    VITE_DISCORD_CLIENT_ID: "1316295472121384991",
+    VITE_TURNSTILE_SITE_KEY: "0x4AAAAAAA18iQyUa3nEBopk",
     VITE_TURNSTILE_BYPASS_SECRET: undefined,
   },
 };
