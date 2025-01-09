b40e494daae255c57ba6a226dae2374d5c0d0eb7
real2two <26689496+real2two@users.noreply.github.com>
2025-01-09 08:50:19 -0500

    feat: made changing audio volume actually work

--- apps/client/src/lib/stores/home/settings.ts
+++ apps/client/src/lib/stores/home/settings.ts
@@ -1,9 +1,5 @@
-import { getCookie, setCookie } from "$lib/utils/cookies";
+import { getCookie } from "$lib/utils/cookies";
 import { writable } from "svelte/store";
 
 const cookieVolume = Number.parseInt(getCookie("volume"));
-export let volumeValue = writable<number>(!cookieVolume || cookieVolume < 0 || cookieVolume > 100 ? 100 : cookieVolume);
-
-volumeValue.subscribe((value) => {
-  setCookie("volume", value.toString());
-});
+export let volumeValue = writable<number>(!cookieVolume || cookieVolume < 0 || cookieVolume > 100 ? 50 : cookieVolume);
--- apps/client/src/lib/utils/audio.ts
+++ apps/client/src/lib/utils/audio.ts
@@ -1,15 +1,43 @@
+import { volumeValue } from "$lib/stores/home/settings";
+import { get } from "svelte/store";
+import { setCookie } from "./cookies";
+
+export const playingAudio = new Set<HTMLAudioElement>();
+
+volumeValue.subscribe((value) => {
+  setCookie("volume", value.toString());
+
+  for (const audio of playingAudio.values()) {
+    audio.volume = value / 100;
+  }
+});
+
 export const audio = {
-  press: loadAudio("/audio/press.mp3", 0.6),
+  press: loadAudio("/audio/press.mp3"),
   close: loadAudio("/audio/close.mp3"),
   start: loadAudio("/audio/start.mp3"),
 };
 
-function loadAudio(fileName: string, volume = 1) {
+function loadAudio(fileName: string) {
   const file = new Audio(fileName);
-  file.volume = volume;
-
   return {
     file,
-    play: () => (file.cloneNode(true) as HTMLAudioElement).play(),
+    play: () => {
+      const clonedAudio = file.cloneNode(true) as HTMLAudioElement;
+      clonedAudio.volume = get(volumeValue) / 100;
+
+      playingAudio.add(clonedAudio);
+
+      clonedAudio.addEventListener("ended", () => {
+        playingAudio.delete(clonedAudio);
+      });
+      clonedAudio.addEventListener("pause", () => {
+        playingAudio.delete(clonedAudio);
+        clonedAudio.currentTime = clonedAudio.duration;
+      });
+      clonedAudio.addEventListener("error", () => playingAudio.delete(clonedAudio));
+
+      clonedAudio.play();
+    },
   };
 }
